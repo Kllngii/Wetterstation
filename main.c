7e0862c2c3724c9b36ac3a97eae46b2f91c8a4c7
Lasse Kelling <lasse_kelling@icloud.com>
2023-10-24 13:21:00 +0200

    Initialpush

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+ * Name: Lasse Kelling, Fabian Schmalenbach
+ * Datum: 24.10.2023
+ *
+ * Unter (Properties -> Resource) encoding UTF-8 und line delimitor Unix auswählen!
+ */
+#include "inc/tm4c1294ncpdt.h"
+#include <stdint.h>
+#include <stdio.h>
+
+void main(), setup();
+
+void main() {
+    setup();
+
+    while(1) {
+
+    }
+}
+
+void setup() {
+    printf("Setup erfolgreich!\n");
+}
