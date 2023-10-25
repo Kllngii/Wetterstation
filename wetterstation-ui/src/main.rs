390c65fdb5614cefb13bb1510f3f9efe30770875
Lasse Kelling <lasse_kelling@icloud.com>
2023-10-25 20:26:56 +0200

    die in-Properties können jetzt aus rust angesprochen werden

--- wetterstation-ui/src/main.rs
+++ wetterstation-ui/src/main.rs
@@ -1,4 +1,10 @@
-use slint::private_unstable_api::re_exports::PointerEvent;
+use chrono::offset::Utc;
+use chrono::DateTime;
+use slint::SharedString;
+use std::time::SystemTime;
+
+use slint::{Timer, TimerMode};
+
 slint::include_modules!();
 #[forbid(unsafe_code)]
 
@@ -13,10 +19,31 @@ fn main() {
     startup();
     let main_window = MainWindow::new().unwrap();
 
-    let a = main_window.global::<CallbackExporter>().on_temperature_touch(|pe: PointerEvent| {
-        println!("{pe:#?}");
+    let weak_main = main_window.as_weak().unwrap();
+    main_window.global::<CallbackExporter>().on_temperature_touch(move || {
+        weak_main.set_show_temperature(!weak_main.get_show_temperature());
     });
 
-    main_window.run().expect("Fehler beim Anzeigen des Fensters!");
+    let weak_main = main_window.as_weak().unwrap();
+    main_window.global::<CallbackExporter>().on_humidity_touch(move || {
+        weak_main.set_show_humidity(!weak_main.get_show_humidity());
+    });
 
+    let weak_main = main_window.as_weak().unwrap();
+    main_window.global::<CallbackExporter>().on_pressure_touch(move || {
+        weak_main.set_show_pressure(!weak_main.get_show_pressure());
+    });
+
+    let clock_refresh_timer = Timer::default();
+    let weak_main = main_window.as_weak().unwrap();
+
+    clock_refresh_timer.start(TimerMode::Repeated, std::time::Duration::from_millis(200), move || {
+        let system_time = SystemTime::now();
+        let datetime: DateTime<Utc> = system_time.into();
+        
+        weak_main.set_time(SharedString::from(datetime.format("%T").to_string()));
+    });
+
+
+    main_window.run().expect("Fehler beim Anzeigen des Fensters!");
 }
