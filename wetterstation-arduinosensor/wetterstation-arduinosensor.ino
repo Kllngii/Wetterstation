342315f3e251e632baf78ebe550ceaadc4876723
Lasse Kelling <lasse_kelling@icloud.com>
2023-11-04 02:29:51 +0100

    kleine Anpassungen, um BME280 und CCS811 Sensordaten abzufragen

--- wetterstation-arduinosensor/wetterstation-arduinosensor.ino
+++ wetterstation-arduinosensor/wetterstation-arduinosensor.ino
@@ -1,18 +1,55 @@
-#include <dhtnew.h>
+#include <Wire.h>
+#include <math.h>
+#include <dhtnew.h> //DHTNew: https://github.com/RobTillaart/DHTNew
+#include <BME280I2C.h> //BME280: https://github.com/finitespace/BME280/tree/master 
+#include <CCS811-SOLDERED.h> //CCS811-Soldered: https://github.com/SolderedElectronics/Soldered-CCS811-Air-Quality-Sensor-Arduino-Library/
 
-DHTNEW dht22Sensor(5);
+DHTNEW dht22Sensor(5); //Temperatur(°C), Luftfeuchtigkeit(%rel)
+BME280I2C bme280Sensor; //Temperatur(°C), Luftfeuchtigkeit(%rel), Luftdruck(hPa)
+CCS_811 ccs811Sensor; //CO2 Konzentration(ppm)[400ppm .. 8192ppm], TVOC Konzentration(ppb)[0ppb .. 1187ppb]
+
+float lastValidTemp = 21, lastValidHumi = 46;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("DHT22");
+  Serial.print("INIT");
+  Wire.begin();
+
+  bme280Sensor.begin();
+  ccs811Sensor.begin();
+
+  Serial.println(" DONE");
 }
 
 void loop() {
   dht22Sensor.read();
-  Serial.print("{\"temperature\":");
+
+  float temp(NAN), hum(NAN), pres(NAN);
+  BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+  BME280::PresUnit presUnit(BME280::PresUnit_hPa);
+  bme280Sensor.read(pres, temp, hum, tempUnit, presUnit);
+
+  if (ccs811Sensor.dataAvailable()) {
+    ccs811Sensor.readAlgorithmResults();
+    lastValidTemp = isnan(temp) ? lastValidTemp : temp;
+    lastValidHumi = isnan(hum) ? lastValidHumi : hum;
+    ccs811Sensor.setEnvironmentalData(lastValidHumi, lastValidTemp); //Sensorabgleich für die nächste Messung anpassen
+  }
+
+  Serial.print("{\"dht-temperature\":");
   Serial.print(dht22Sensor.getTemperature(), 1);
-  Serial.print(",\"humidity\":");
+  Serial.print(",\"dht-humidity\":");
   Serial.print(dht22Sensor.getHumidity(), 1);
+  Serial.print(",\"bme-temperature\":");
+  Serial.print(temp, 1);
+  Serial.print(",\"bme-humidity\":");
+  Serial.print(hum, 1);
+  Serial.print(",\"bme-pressure\":");
+  Serial.print(pres, 1);
+  Serial.print(",\"ccs-co2\":");
+  Serial.print(ccs811Sensor.getCO2(), 1);
+  Serial.print(",\"ccs-tvoc\":");
+  Serial.print(ccs811Sensor.getTVOC(), 1);
   Serial.println("}");
 
   delay(2000);
--- wetterstation-ui/src/ringtest.rs
+++ wetterstation-ui/src/ringtest.rs
@@ -1,22 +1,22 @@
 use ringbuffer::{AllocRingBuffer, RingBuffer};
 
-use crate::DHT_Data;
+use crate::SensorData;
 
 pub(crate) fn run() {
-    let mut buffer = AllocRingBuffer::<DHT_Data>::new(10);
+    let mut buffer = AllocRingBuffer::<SensorData>::new(10);
     buffer.fill_default();
-    buffer.push(DHT_Data { temperature: Some(0f64), humidity: Some(0f64) });
-    buffer.push(DHT_Data { temperature: Some(1f64), humidity: Some(1f64) });
-    buffer.push(DHT_Data { temperature: Some(2f64), humidity: Some(2f64) });
-    buffer.push(DHT_Data { temperature: Some(3f64), humidity: Some(3f64) });
-    buffer.push(DHT_Data { temperature: Some(4f64), humidity: Some(4f64) });
-    buffer.push(DHT_Data { temperature: Some(5f64), humidity: Some(5f64) });
-    buffer.push(DHT_Data { temperature: Some(6f64), humidity: Some(6f64) });
-    buffer.push(DHT_Data { temperature: Some(7f64), humidity: Some(7f64) });
-    buffer.push(DHT_Data { temperature: Some(8f64), humidity: Some(8f64) });
-    buffer.push(DHT_Data { temperature: Some(9f64), humidity: Some(9f64) });
-    buffer.push(DHT_Data { temperature: Some(10f64), humidity: Some(10f64) });
-    buffer.push(DHT_Data { temperature: Some(11f64), humidity: Some(11f64) });
-    buffer.push(DHT_Data { temperature: Some(12f64), humidity: Some(12f64) });
+    buffer.push(SensorData { temperature: Some(0f64), humidity: Some(0f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(1f64), humidity: Some(1f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(2f64), humidity: Some(2f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(3f64), humidity: Some(3f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(4f64), humidity: Some(4f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(5f64), humidity: Some(5f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(6f64), humidity: Some(6f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(7f64), humidity: Some(7f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(8f64), humidity: Some(8f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(9f64), humidity: Some(9f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(10f64), humidity: Some(10f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(11f64), humidity: Some(11f64), ..Default::default()});
+    buffer.push(SensorData { temperature: Some(12f64), humidity: Some(12f64), ..Default::default()});
     buffer.iter().for_each(|x| {println!("{:#?}", x);});
 }
