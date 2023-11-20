194fc509c7b4bbc47c9ab2f7542181098ec90168
Lasse Kelling <lasse@kelling.de>
2023-11-20 10:25:35 +0100

    hübsche UI

--- slint-pico/src/main.rs
+++ slint-pico/src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![cfg_attr(not(feature = "simulator"), no_main)]
+#[cfg(not(feature = "simulator"))]
+mod xpt2046;
 
 extern crate alloc;
 
@@ -9,9 +11,10 @@ fn create_slint_app() -> AppWindow {
     let ui = AppWindow::new().expect("Failed to load UI");
 
     let ui_handle = ui.as_weak();
+
     ui.on_request_increase_value(move || {
         let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+        //ui.set_counter(ui.get_counter() + 1);
     });
 
     ui
@@ -83,6 +86,7 @@ fn main() -> ! {
     let cs = pins.gpio9.into_push_pull_output();
     let di = display_interface_spi::SPIInterface::new(spi.acquire_spi(), dc, cs);
     let mut display = st7789::ST7789::new(di, Some(rst), Some(bl), 320, 240);
+
 
     display.init(&mut delay).unwrap();
     display.set_orientation(st7789::Orientation::Landscape).unwrap();
@@ -190,126 +194,11 @@ fn main() -> ! {
             continue;
         }
 
+
+
         // TODO: we could save battery here by going to sleep up to
         //   slint::platform::duration_until_next_timer_update()
         // or until the next touch interrupt, whatever comes first
         // cortex_m::asm::wfe();
-    }
-}
-
-#[cfg(not(feature = "simulator"))]
-mod xpt2046 {
-    use embedded_hal::blocking::spi::Transfer;
-    use embedded_hal::digital::v2::{InputPin, OutputPin};
-    use fugit::RateExtU32;
-
-    pub struct XPT2046<IRQ: InputPin + 'static, CS: OutputPin, SPI: Transfer<u8>> {
-        irq: IRQ,
-        cs: CS,
-        spi: SPI,
-        pressed: bool,
-    }
-
-    impl<PinE, IRQ: InputPin<Error = PinE>, CS: OutputPin<Error = PinE>, SPI: Transfer<u8>>
-        XPT2046<IRQ, CS, SPI>
-    {
-        pub fn new(irq: IRQ, mut cs: CS, spi: SPI) -> Result<Self, PinE> {
-            cs.set_high()?;
-            Ok(Self { irq, cs, spi, pressed: false })
-        }
-
-        pub fn read(&mut self) -> Result<Option<(f32, f32)>, Error<PinE, SPI::Error>> {
-            const PRESS_THRESHOLD: i32 = -25_000;
-            const RELEASE_THRESHOLD: i32 = -30_000;
-            let threshold = if self.pressed { RELEASE_THRESHOLD } else { PRESS_THRESHOLD };
-            self.pressed = false;
-
-            if self.irq.is_low().map_err(|e| Error::Pin(e))? {
-                const CMD_X_READ: u8 = 0b10010000;
-                const CMD_Y_READ: u8 = 0b11010000;
-                const CMD_Z1_READ: u8 = 0b10110000;
-                const CMD_Z2_READ: u8 = 0b11000000;
-
-                // These numbers were measured approximately.
-                const MIN_X: u32 = 1900;
-                const MAX_X: u32 = 30300;
-                const MIN_Y: u32 = 2300;
-                const MAX_Y: u32 = 30300;
-
-                // FIXME! how else set the frequency to this device
-                unsafe { set_spi_freq(3_000_000u32.Hz()) };
-
-                self.cs.set_low().map_err(|e| Error::Pin(e))?;
-
-                macro_rules! xchg {
-                    ($byte:expr) => {
-                        match self
-                            .spi
-                            .transfer(&mut [$byte, 0, 0])
-                            .map_err(|e| Error::Transfer(e))?
-                        {
-                            [_, h, l] => ((*h as u32) << 8) | (*l as u32),
-                            _ => return Err(Error::InternalError),
-                        }
-                    };
-                }
-
-                let z1 = xchg!(CMD_Z1_READ);
-                let z2 = xchg!(CMD_Z2_READ);
-                let z = z1 as i32 - z2 as i32;
-
-                if z < threshold {
-                    xchg!(0);
-                    self.cs.set_high().map_err(|e| Error::Pin(e))?;
-                    unsafe { set_spi_freq(62_500_000.Hz()) };
-                    return Ok(None);
-                }
-
-                xchg!(CMD_X_READ | 1); // Dummy read, first read is a outlier
-
-                let mut point = (0u32, 0u32);
-                for _ in 0..10 {
-                    let y = xchg!(CMD_Y_READ);
-                    let x = xchg!(CMD_X_READ);
-                    point.0 += i16::MAX as u32 - x;
-                    point.1 += y;
-                }
-
-                let z1 = xchg!(CMD_Z1_READ);
-                let z2 = xchg!(CMD_Z2_READ);
-                let z = z1 as i32 - z2 as i32;
-
-                xchg!(0);
-                self.cs.set_high().map_err(|e| Error::Pin(e))?;
-                unsafe { set_spi_freq(62_500_000.Hz()) };
-
-                if z < RELEASE_THRESHOLD {
-                    return Ok(None);
-                }
-
-                point.0 /= 10;
-                point.1 /= 10;
-                self.pressed = true;
-                Ok(Some((
-                    point.0.saturating_sub(MIN_X) as f32 / (MAX_X - MIN_X) as f32,
-                    point.1.saturating_sub(MIN_Y) as f32 / (MAX_Y - MIN_Y) as f32,
-                )))
-            } else {
-                Ok(None)
-            }
-        }
-    }
-
-    pub enum Error<PinE, TransferE> {
-        Pin(PinE),
-        Transfer(TransferE),
-        InternalError,
-    }
-
-    unsafe fn set_spi_freq(freq: impl Into<fugit::Hertz<u32>>) {
-        use rp_pico::hal;
-        // FIXME: the touchscreen and the LCD have different frequencies, but we cannot really set different frequencies to different SpiProxy without this hack
-        hal::spi::Spi::<_, _, 8>::new(hal::pac::Peripherals::steal().SPI1)
-            .set_baudrate(125_000_000u32.Hz(), freq);
     }
 }
