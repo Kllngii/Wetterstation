9b74fd291fde6e4830458974a466440bbea62f0e
Fabian Schmalenbach <fabian.schmalenbach@gmail.com>
2023-11-25 12:53:41 +0100

    Added BME280 transmit

--- arduino/433MHz-arduinotest/Receiver/Receiver.ino
+++ arduino/433MHz-arduinotest/Receiver/Receiver.ino
@@ -9,7 +9,7 @@ bool On = false;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   
   if(!driver.init())
@@ -24,32 +24,11 @@ void setup() {
 
 void loop() {
     uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-    char charBuf[4];
-    charBuf[4] = '\0';
     uint8_t buflen = sizeof(buf);
 
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
-    // Message with a good checksum received, dump it.
-    charBuf[2] = buf[2];
-    charBuf[1] = buf [1];
-    charBuf[0] = buf [0];
-    driver.printBuffer("Got:", buf, buflen);
-
-    if (buf[0] == 'O')
-    {
-      if (buf[1] == 'n')
-      {
-        digitalWrite(LED_BUILTIN, LOW);
-      }
-      else if (buf[1] == 'f')
-      {
-        if(buf[2] == 'f')
-        {
-          digitalWrite(LED_BUILTIN, HIGH);
-        } 
-      }
-    }
+      Serial.println((char*)buf);
 
     
     }
--- arduino/433MHz-arduinotest/Transmitter/Transmitter.ino
+++ arduino/433MHz-arduinotest/Transmitter/Transmitter.ino
@@ -1,15 +1,24 @@
 #include <RH_ASK.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 #define D0  16
 #define D1  5
 #define D2  4
 
+Adafruit_BME280 bme;
 RH_ASK driver(2000, D1, D0, D2, false);
-bool On = false;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  if(!bme.begin(0x76))
+  {
+    Serial.println("No sensor found");
+  }
+  
   if(!driver.init())
   {
     Serial.println("Init failed");
@@ -22,21 +31,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  const char *msgOn = "On";
-  const char *msgOff = "Off";
+  String tempString = "Temp: " + (String)bme.readTemperature() + " °C";
+  String pressString = "Pressure: " + (String)(bme.readPressure() / 100.0f) + " hPa";
+  String humString = "Humidity: " + (String)bme.readHumidity() + " %";
+  const char *msgTemp = tempString.c_str();
+  const char *msgPress = pressString.c_str();
+  const char *msgHum = humString.c_str();
 
-  if (On)
-  {
-    driver.send((uint8_t *)msgOff, strlen(msgOff));
-    driver.waitPacketSent();
-    On = false;
-  }
-  else
-  {
-    driver.send((uint8_t *)msgOn, strlen(msgOn));
-    driver.waitPacketSent();
-    On = true;
-  }
-  delay(2000);
+  driver.send((uint8_t *)msgTemp, strlen(msgTemp));
+  driver.waitPacketSent();
+  driver.send((uint8_t *)msgPress, strlen(msgPress));
+  driver.waitPacketSent();
+  driver.send((uint8_t *)msgHum, strlen(msgHum));
+  driver.waitPacketSent();
+  delay(500);
 
 }
