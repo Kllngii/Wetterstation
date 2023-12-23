9a915cd4241c41fad3e056a8ee4cbb916e2fe8ba
Fabian Schmalenbach <fabian.schmalenbach@gmail.com>
2023-12-23 23:18:59 +0100

    Switched back to SoftwareSerial

--- externalSensor_esp/externalSensor_esp.ino
+++ externalSensor_esp/externalSensor_esp.ino
@@ -2,60 +2,139 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include "DCF77.h"
 #include "DataTypes.h"
 
-#define DCF_DATA_PIN        14  // D5
-#define DCF_INTERRUPT_PIN   14  // D5
-#define SET_PIN             2   // D4
+#define DCF_DATA_PIN 14      // D5
+#define DCF_INTERRUPT_PIN 14 // D5
+#define SET_PIN 12           // D6
+#define HC_RX 13             // D7
+#define HC_TX 15             // D8
 
-#define SENSOR_SEND_FREQUENCY_MILLIS    60000
-#define TIME_SEND_FREQUENCY_MILLIS  120000
+#define SENSOR_SEND_FREQUENCY_MILLIS 60000
+#define TIME_SEND_FREQUENCY_MILLIS 120000
+
+#define HC12_INIT_DELAY 2000
 
 Adafruit_BME280 bme;
 DCF77 DCF = DCF77(DCF_DATA_PIN, DCF_INTERRUPT_PIN);
+SoftwareSerial HC12(HC_RX, HC_TX);
 
-TimeSendBuf timeBuffer = {'T','I','M','E'};
-BMESendBuf sensorBuffer = {'E','B','M','E'};  
+TimeSendBuf timeBuffer = {'T', 'I', 'M', 'E'};
+BMESendBuf sensorBuffer = {'E', 'B', 'M', 'E'};
 
 bool timeValid = false;
 unsigned long lastSensorSend = 0;
 unsigned long lastTimeSend = 0;
 
-void setup() {
+void setup()
+{
+    int hcInitTime;
     pinMode(SET_PIN, INPUT);
-    Serial.begin(9600);
+    Serial.begin(115200);
+    HC12.begin(9600);
     WiFi.mode(WIFI_OFF);
 
     // Start i2c communication
-    if(!bme.begin(0x76))
+    if (!bme.begin(0x76))
     {
         Serial.println("No sensor found");
     }
-    
+
     // Enable Setting Mode in HC-12
     pinMode(SET_PIN, OUTPUT);
 
     // Set Baudrate to 9600
     delay(100);
-    Serial.print("AT+B9600");
+    HC12.print("AT+B9600");
+    hcInitTime = millis();
+    while (HC12.available() < 8)
+    {
+        delay(1); // To reset watchdog
+        if ((millis() - hcInitTime) > HC12_INIT_DELAY)
+        {
+            ESP.restart();
+        }
+    }
+    for (int i = 0; i < 8; i++)
+    {
+        Serial.print((char)HC12.read());
+    }
+    Serial.println("");
 
     // Set Channel to 1
     delay(100);
-    Serial.print("AT+C001");
+    HC12.print("AT+C001");
+    hcInitTime = millis();
+    while (HC12.available() < 7)
+    {
+        delay(1);
+        if ((millis() - hcInitTime) > HC12_INIT_DELAY)
+        {
+            ESP.restart();
+        }
+    }
+    for (int i = 0; i < 7; i++)
+    {
+        Serial.print((char)HC12.read());
+    }
+    Serial.println("");
 
     // Set transmission mode to 3
     delay(100);
-    Serial.print("AT+FU3");
+    HC12.print("AT+FU3");
+    hcInitTime = millis();
+    while (HC12.available() < 6)
+    {
+        delay(1);
+        if ((millis() - hcInitTime) > HC12_INIT_DELAY)
+        {
+            ESP.restart();
+        }
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        Serial.print((char)HC12.read());
+    }
+    Serial.println("");
 
     // Set Power to 8dBm
     delay(100);
-    Serial.print("AT+P6");
+    HC12.print("AT+P4");
     delay(100);
+    hcInitTime = millis();
+    while (HC12.available() < 5)
+    {
+        delay(1);
+        if ((millis() - hcInitTime) > HC12_INIT_DELAY)
+        {
+            ESP.restart();
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Serial.print((char)HC12.read());
+    }
+    Serial.println("");
 
     // Set data transmission to 8 bits + odd parity + 1 stop bit
     Serial.print("AT+U8N1");
+    hcInitTime = millis();
+    while (HC12.available() < 7)
+    {
+        delay(1);
+        if ((millis() - hcInitTime) > HC12_INIT_DELAY)
+        {
+            ESP.restart();
+        }
+    }
+    for (int i = 0; i < 7; i++)
+    {
+        Serial.print((char)HC12.read());
+    }
+    Serial.println("");
 
     // Disable Setting Mode in HC-12
     pinMode(SET_PIN, INPUT);
@@ -67,34 +146,37 @@ void setup() {
     DCF.Start();
 }
 
-void loop() {
+void loop()
+{
     time_t DCFtime = DCF.getTime();
-    if(DCFtime != 0)
+    if (DCFtime != 0)
     {
         // Set internal time to dcf time if valid
         setTime(DCFtime);
         if (!timeValid)
         {
-          // Reset timestamps because time changed
-          lastSensorSend = millis();
-          lastTimeSend = millis();
+            // Reset timestamps because time changed
+            lastSensorSend = millis();
+            lastTimeSend = millis();
         }
-        timeValid = true;   // Begin transmission of time
+        timeValid = true; // Begin transmission of time
         timeBuffer.data.hour = hour();
         timeBuffer.data.minute = minute();
         timeBuffer.data.second = second();
         timeBuffer.data.year = year();
         timeBuffer.data.month = month();
         timeBuffer.data.day = day();
-        Serial.write((const uint8_t*)timeBuffer.buf, sizeof(timeBuffer));
+        HC12.write((const uint8_t *)timeBuffer.buf, sizeof(timeBuffer));
+        Serial.println("Sent DCF Time");
     }
 
     if (DCF.isMeteoReady())
     {
         MeteoRawSendBuf sendBuffer = DCF.getMeteoBuffer();
-        Serial.write((const uint8_t*)sendBuffer.buf, sizeof(sendBuffer));
+        HC12.write((const uint8_t *)sendBuffer.buf, sizeof(sendBuffer));
+        Serial.println("Sent Meteodata");
     }
-    
+
     // Send BME Data
     if ((millis() - lastSensorSend) > SENSOR_SEND_FREQUENCY_MILLIS)
     {
@@ -102,8 +184,9 @@ void loop() {
         sensorBuffer.data.temp = bme.readTemperature();
         sensorBuffer.data.humidity = bme.readHumidity();
         sensorBuffer.data.pressure = bme.readPressure();
-        Serial.write((const uint8_t*)sensorBuffer.buf, sizeof(sensorBuffer));
+        HC12.write((const uint8_t *)sensorBuffer.buf, sizeof(sensorBuffer));
         lastSensorSend = millis();
+        Serial.println("Sent BME Data");
     }
     // Send DCF Data
     /*
